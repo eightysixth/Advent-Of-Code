737ca45f191ef09fc7d0e05dab75b29a38a40851
eightysixth <25541207+eightysixth@users.noreply.github.com>
2022-12-03 15:06:56 -0700

    2021-1

---
+++ 2021/Day 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Part A
+string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+int lastDepth = -1;
+int largerThanPrevious = 0;
+for (int i = 0; i < lines.Length; i++){
+    if (i == 0){
+        lastDepth = int.Parse(lines[i]);
+    } else {
+        int currentDepth = int.Parse(lines[i]);
+        
+        if (currentDepth > lastDepth){
+            largerThanPrevious += 1;
+        }
+        lastDepth = currentDepth;
+    }
+}
+
+Console.WriteLine("Part A Answer: " + largerThanPrevious.ToString()); //1553
+
+
+int previousWindowSum = int.MaxValue;
+int largerWindowThanPrevious = 0;
+int windowSize = 3;
+
+for (int i = 0; i < lines.Length; i++){
+    int currentWindowSum = 0;
+    if (i + windowSize > lines.Length){
+        break;
+    }
+    for (int j = 0; j < windowSize; j++){
+        currentWindowSum += int.Parse(lines[i+j]);
+    }
+    if (currentWindowSum > previousWindowSum){
+        largerWindowThanPrevious += 1;
+    }
+    previousWindowSum = currentWindowSum;
+}
+Console.WriteLine("Part B Answer: " + largerWindowThanPrevious.ToString()); //1597
